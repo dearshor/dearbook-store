315cb6f435f51f8c5bf6b89edc6fe8772658bbee
Dearshor Xue <dearshor@gmail.com>
2013-05-15 23:18:33 +0800

    changed to use spring factoryBean's getObject() method to renturn instance of factoryBean instand of factoryBean itself.
    Signed-off-by: Dearshor Xue <dearshor@gmail.com>

--- src/main/java/com/dearshor/dearbook/config/Infrastructure.java
+++ src/main/java/com/dearshor/dearbook/config/Infrastructure.java
@@ -2,13 +2,14 @@ package com.dearshor.dearbook.config;
 
 import static org.springframework.orm.jpa.vendor.Database.HSQL;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactoryBean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -18,7 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Infrastructure {
 	
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	public EntityManagerFactory entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
 		emf.setDataSource(dataSource());
 		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
@@ -27,22 +28,36 @@ public class Infrastructure {
 		jpaVendorAdapter.setShowSql(true);
 		emf.setJpaVendorAdapter(jpaVendorAdapter);
 		emf.setPersistenceUnitName("com.dearshor.dearsbook.persistence-unit.dev");
-		return emf;
+		emf.afterPropertiesSet();
+		return emf.getObject();
 	}
 	
-	@Bean(destroyMethod = "shutdown")
+	@Bean
 	public DataSource dataSource() {
-		// TODO Auto-generated method stub
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-	    EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).build();
-		return db;
+		EmbeddedDatabaseFactoryBean dbf = new EmbeddedDatabaseFactoryBean();
+		dbf.setDatabaseType(EmbeddedDatabaseType.HSQL);
+		dbf.afterPropertiesSet();
+		return dbf.getObject();
 	}
 
 	@Bean
 	public JpaTransactionManager transactionManager() {
-		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(entityManagerFactory());
 		return jpaTransactionManager;
 		
 	}
+	
+	/**
+	 * <p>While configing a bean of type {@link javax.persistence.EntityManagerFactory}, it's necessary for creating {@link org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor}, 
+	 * otherwise it cause {@link java.lang.IllegalStateException}: No persistence exception translators found in bean factory. Cannot perform exception translation.</p>
+	 * 
+	 * <p>Oppositely, for instance, while configing a bean of type LocalContainerEntityManagerFactoryBean, it's unnecessary. why?</p>
+	 * 
+	 * @return instance of {@link org.springframework.orm.hibernate4.HibernateExceptionTranslator}
+	 */
+	@Bean 
+    public HibernateExceptionTranslator hibernateExceptionTranslator(){ 
+      return new HibernateExceptionTranslator(); 
+    }
 
 }
